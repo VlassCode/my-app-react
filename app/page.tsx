2035093989808cc1dc2e3e244d27dbc38e208cc0
Vladimir Sarmiento <vlass.general@gmail.com>
2025-04-21 17:06:24 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,9 @@ export default function Home() {
             Get started by editing <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>
+            <strong>This React project is by VlassCode.</strong>
+          </li>
         </ol>
 
         <div className={styles.ctas}>
